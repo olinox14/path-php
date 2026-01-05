541859472a703b403ed79c1439e16cd94c7b20c2
olinox14 <olinox14@tuta.io>
2026-01-05 11:36:13 +0100

    Updates Docker image and applies code styling
    Updates the Dockerfile to use PHP 8.5.
    
    Applies code styling using PHP-CS-Fixer for improved code readability and consistency.
    
    Also, it makes the previous exception parameter nullable for better compatibility with newer PHP versions.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM php:8.3-cli
+FROM php:8.5-cli
 
 # Installer les dépendances pour l'extension zip
 RUN apt-get update && apt-get install -y \
--- src/Exception/FileExistsException.php
+++ src/Exception/FileExistsException.php
@@ -7,7 +7,7 @@ class FileExistsException extends \Exception
     public function __construct(
         string $message = "File already exists",
         int $code = 0,
-        \Exception $previous = null
+        ?\Exception $previous = null
     ) {
         parent::__construct($message, $code, $previous);
     }
